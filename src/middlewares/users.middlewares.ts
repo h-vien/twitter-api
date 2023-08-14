6a2e2aacad0d64ca1be3ca5305e6c8608c3a88bd
Vien Huynh <vien200203@gmail.com>
2023-08-14 09:08:26 +0700

    feat: validator register api

--- src/middlewares/users.middlewares.ts
+++ src/middlewares/users.middlewares.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from 'express'
+import { checkSchema } from 'express-validator'
 
 export const loginValidator = (req: Request, res: Response, next: NextFunction) => {
   const { email, password } = req.body
@@ -9,3 +10,76 @@ export const loginValidator = (req: Request, res: Response, next: NextFunction) 
   }
   next()
 }
+
+export const registerValidator = checkSchema({
+  name: {
+    isString: true,
+    isLength: {
+      errorMessage: 'Name should be at least 2 chars long',
+      options: { min: 2, max: 20 }
+    },
+    notEmpty: {
+      errorMessage: 'Name is required'
+    },
+    trim: true
+  },
+  email: {
+    isEmail: true,
+    trim: true,
+    notEmpty: true
+  },
+  password: {
+    isString: true,
+    notEmpty: true,
+    isLength: {
+      options: {
+        min: 6,
+        max: 20
+      }
+    },
+    isStrongPassword: {
+      options: {
+        minLength: 6,
+        minLowercase: 1,
+        minUppercase: 1,
+        minNumbers: 1,
+        minSymbols: 1
+      }
+    }
+  },
+  confirm_password: {
+    isString: true,
+    notEmpty: true,
+    isLength: {
+      options: {
+        min: 6,
+        max: 20
+      }
+    },
+    isStrongPassword: {
+      options: {
+        minLength: 6,
+        minLowercase: 1,
+        minUppercase: 1,
+        minNumbers: 1,
+        minSymbols: 1
+      }
+    },
+    custom: {
+      options: (value, { req }) => {
+        if (value !== req.body.password) {
+          throw new Error('Password confirmation does not match password')
+        }
+        return true
+      }
+    }
+  },
+  date_of_birth: {
+    isISO8601: {
+      options: {
+        strict: true,
+        strictSeparator: true
+      }
+    }
+  }
+})
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import { loginController, registerController } from '~/controllers/users.controller'
-import { loginValidator } from '~/middlewares/users.middlewares'
+import { loginValidator, registerValidator } from '~/middlewares/users.middlewares'
+import { validate } from '~/utils/validations'
 
 const usersRouter = Router()
 
 usersRouter.post('/login', loginValidator, loginController)
-usersRouter.post('/register', registerController)
+
+usersRouter.post('/register', validate(registerValidator), registerController)
 
 export default usersRouter
