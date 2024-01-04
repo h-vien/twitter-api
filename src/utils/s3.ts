3b8fd7b4a0c1376fdeed4b40c94c24c65acabda4
Vien Huynh <vien200203@gmail.com>
2024-01-04 22:46:21 +0700

    feat(s3): test upload image to s3

--- src/utils/s3.ts
+++ src/utils/s3.ts
@@ -1,13 +1,35 @@
 import { S3 } from '@aws-sdk/client-s3'
+import { Upload } from '@aws-sdk/lib-storage'
 import { config } from 'dotenv'
+import fs from 'fs'
+import path from 'path'
 
 config()
 const s3 = new S3({
-  region: process.env.AWS_REGION,
   credentials: {
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
     accessKeyId: process.env.AWS_ACCESS_KEY_ID as string
   }
 })
 
-s3.listBuckets({}).then((data) => console.log(data.Buckets))
+const file = fs.readFileSync(path.resolve('uploads/images/39fa1f3774eb0c1eb478aaa002409.jpg'))
+
+const parallelUploads3 = new Upload({
+  client: s3,
+  params: { Bucket: 'twitter-s3-dlone', Key: 'anh1.jpg', Body: file, ContentType: 'image/jpeg' },
+
+  tags: [
+    /*...*/
+  ], // optional tags
+  queueSize: 4, // optional concurrency configuration
+  partSize: 1024 * 1024 * 5, // optional size of each part, in bytes, at least 5MB
+  leavePartsOnError: false // optional manually handle dropped parts
+})
+
+parallelUploads3.on('httpUploadProgress', (progress) => {
+  console.log(progress)
+})
+
+parallelUploads3.done().then((res) => {
+  console.log(res)
+})
